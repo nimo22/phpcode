189bb44313fb13999711acf424ef489cae89b8a7
nimo22 <berry.mohamad-ali@courrier.uqam.ca>
2020-10-28 04:42:26 -0400

    primative float + form

---
+++ floatexemple.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <style>
+        .left {
+            float: left;
+            margin: 2px 3px 2px 0px;
+            padding: 20px 20px;
+            border: solid black;
+            width: 150px;
+            height: 300px;
+            text-align: center;
+        }
+
+        .right {
+
+            float: right;
+            margin: 2px 0px 1px 2px;
+            padding: 20px 20px;
+            height: 150px;
+            width: 200px;
+            border: solid black;
+
+
+        }
+
+        .center {
+            float: auto;
+            margin: 0px 2px 2px 2px;
+            border: solid blue;
+            width: 150px;
+            height: 100px;
+            padding: 20px 20px;
+            border: solid black 5px;
+
+
+
+
+        }
+    </style>
+
+</head>
+
+<body>
+    <div class="right">
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. At aspernatur placeat provident iure? Aperiam eos nemo enim minus sit officia? Quas unde ipsam officia alias fugiat placeat voluptas, vel expedita.</p>
+
+    </div>
+    <div class="left">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ea ut eius ipsa, numquam dolor assumenda aperiam doloremque temporibus voluptas iure fugit nemo commodi deleniti quod vitae eum explicabo modi!</p>
+    </div>
+    <div >
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut assumenda officia tempora ullam illum voluptatem esse sed minus? Architecto, quis dolorum veritatis praesentium maiores quae ipsam excepturi fuga totam dicta?<p>
+    </div>
+</body>
+
+
+
+</html>
